b0a8b265bd79ec5e668a007474bfd59fa8ea2cab
Justin Loutsenhizer <nano351@gmail.com>
2013-06-27 21:20:53 -0400

    removed include from config.php of google_php

--- public_html/google_php/config.php
+++ public_html/google_php/config.php
@@ -15,8 +15,6 @@
  * limitations under the License.
  */
 
-include "../configuration.php";
-
 global $apiConfig;
 $apiConfig = array(
     // True if objects should be returned by the service classes.
