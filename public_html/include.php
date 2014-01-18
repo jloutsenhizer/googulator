0ddf123eb178f84c9cc521f1765e55cdb9890d2f
Justin Loutsenhizer <nano351@gmail.com>
2014-01-18 00:02:23 -0500

    -Moved google_php to a php/lib -Added SwiftMailer Library -Added support to send emails via smtp

--- public_html/include.php
+++ public_html/include.php
@@ -1,5 +1,6 @@
 <?php
-require_once "google_php/Google_Client.php";
+require_once "php/lib/google_php/Google_Client.php";
+require_once "php/lib/SwiftMailer/lib/swift_required.php";
 require_once "configuration.php";
 
 function getGoogleId($access_token){
@@ -126,14 +127,55 @@ function outputDirectoryListing($directory,$recursive = true){
     }
 }
 
+$emailer_transport = null;
+$emailer_mailer = null;
+
 function sendEmail($to,$message){
-    global $PREFERRED_HOSTNAME, $UPDATE_EMAIL_ADDRESS;
-    $headers = 'From: ' . $UPDATE_EMAIL_ADDRESS   . "\r\n" .
+    global $PREFERRED_HOSTNAME, $UPDATE_EMAIL_ADDRESS, $UPDATE_EMAIL_NAME, $UPDATE_MAIL_SERVER, $UPDATE_MAIL_SERVER_PORT,
+           $UPDATE_MAIL_SERVER_SSL, $UPDATE_MAIL_SERVER_USERNAME, $UPDATE_MAIL_SERVER_PASSWORD, $emailer_transport, $emailer_mailer;
+
+    if (!isset($emailer_transport)){
+        if (!isset($emailer_transport)){
+            if (!isset($UPDATE_MAIL_SERVER)) {
+                $emailer_transport = Swift_SmtpTransport::newInstance();
+            }
+            else{
+                if ($UPDATE_MAIL_SERVER_SSL){
+                    $emailer_transport = Swift_SmtpTransport::newInstance($UPDATE_MAIL_SERVER, $UPDATE_MAIL_SERVER_PORT, 'ssl');
+                }
+                else{
+                    $emailer_transport = Swift_SmtpTransport::newInstance($UPDATE_MAIL_SERVER, $UPDATE_MAIL_SERVER_PORT);
+                }
+                $emailer_transport->setUsername($UPDATE_MAIL_SERVER_USERNAME)
+                    ->setPassword($UPDATE_MAIL_SERVER_PASSWORD);
+            }
+
+        }
+    }
+
+
+    $emailMessage = Swift_Message::newInstance()
+        ->setSubject("New Updates to Googulator")
+        ->setFrom(array($UPDATE_EMAIL_ADDRESS => $UPDATE_EMAIL_NAME))
+        ->setTo($to)
+        ->setBody($message . "\r\n\r\n" . "Tired of these messages? Use the following url to unsubscribe:\r\n"
+            . "http://$PREFERRED_HOSTNAME/?unsubscribe=" . encodeURIComponent($to));
+
+    if (!isset($emailer_mailer)){
+        $emailer_mailer = Swift_Mailer::newInstance($emailer_transport);
+    }
+
+    return $emailer_mailer->send($emailMessage);
+
+
+
+
+    /*$headers = 'From: ' . $UPDATE_EMAIL_ADDRESS   . "\r\n" .
         'Reply-To: ' . $UPDATE_EMAIL_ADDRESS  . "\r\n" .
         'X-Mailer: PHP/' . phpversion();;
     $message = $message . "\r\n\r\n" . "Tired of these messages? Use the following url to unsubscribe:\r\n"
         . "http://$PREFERRED_HOSTNAME/?unsubscribe=" . encodeURIComponent($to);
-    return mail($to,"New Updates to Googulator",$message,$headers);
+    return mail($to,"New Updates to Googulator",$message,$headers); */
 }
 
 function encodeURIComponent($str) {
--- public_html/php/admin/dispatchUpdateEmail.php
+++ public_html/php/admin/dispatchUpdateEmail.php
@@ -14,19 +14,22 @@ $google_id = getGoogleId($google_token);
 $con = mysql_connect($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD);
 mysql_select_db($MYSQL_DATABASE, $con);
 
+$numEmailsPerSession = 1;
+
 if (hasRole($google_id,"ROLE_ADMIN",$con)){
     $startPosition = $_GET["startId"];
     if ($startPosition == NULL){
         $startPosition = 0;
     }
-    $query = mysql_query("select * from emailUpdates where id>$startPosition limit 1;",$con);
+    $query = mysql_query("select * from emailUpdates where id>$startPosition order by id asc limit $numEmailsPerSession;",$con);
     $numRows = mysql_num_rows($query);
     $lastId = $startPosition;
     while ($row = mysql_fetch_assoc($query)){
+        //TODO: do something (not sure what) when an email fails to send
         sendEmail($row["email"],$_GET["message"]);
         $lastId = $row["id"];
     }
-    echo json_encode(["status"=>"success","done"=>($numRows < 1),"lastId"=>$lastId,"numSent" => $numRows]);
+    echo json_encode(["status"=>"success","done"=>($numRows < $numEmailsPerSession),"lastId"=>$lastId,"numSent" => $numRows]);
 }
 else{
     echo json_encode(["status"=>"failed","error"=>"PERMISSION_DENIED"]);
