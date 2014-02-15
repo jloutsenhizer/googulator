e9d47a9e2ea57489d7bbad237722063fcdfd0779
Justin Loutsenhizer <nano351@gmail.com>
2014-02-15 18:33:47 -0500

    -when the tab with googulator is hidden, the game is paused and then resumed when unhidden

--- public_html/js/modules/play/Module.js
+++ public_html/js/modules/play/Module.js
@@ -213,6 +213,52 @@ define(["gbcore/Gameboy", "nescore/nes","modules/play/DummyApp"], function(Gameb
             currentApp.handleMouseEvent(event);
     });
 
+    //listen for visibility events
+    (function() {
+        var hidden = "hidden";
+
+        // Standards:
+        if (hidden in document)
+            document.addEventListener("visibilitychange", onchange);
+        else if ((hidden = "mozHidden") in document)
+            document.addEventListener("mozvisibilitychange", onchange);
+        else if ((hidden = "webkitHidden") in document)
+            document.addEventListener("webkitvisibilitychange", onchange);
+        else if ((hidden = "msHidden") in document)
+            document.addEventListener("msvisibilitychange", onchange);
+        // IE 9 and lower:
+        else if ('onfocusin' in document)
+            document.onfocusin = document.onfocusout = onchange;
+        // All others:
+        else
+            window.onpageshow = window.onpagehide
+                = window.onfocus = window.onblur = onchange;
+
+        function onchange (evt) {
+            var v = true, h = false,
+                evtMap = {
+                    focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+                };
+
+            evt = evt || window.event;
+            var visible;
+            if (evt.type in evtMap)
+                visible = evtMap[evt.type];
+            else
+                visible = this[hidden] ? h : v;
+            if (active){
+                if (visible){
+                    currentApp.resume();
+                }
+                else{
+                    currentApp.clearButtonStates();
+                    currentApp.pause();
+                }
+
+            }
+        }
+    })();
+
 
     var checkFrames = function(){
         if (container != null && active){
