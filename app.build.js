2349a5a4cf1179f55b60683fb5c1f88fe2a6c775
Justin Loutsenhizer <nano351@gmail.com>
2014-04-05 01:23:34 -0400

    -added basic 404 document -configured uglify2

--- app.build.js
+++ app.build.js
@@ -2,18 +2,42 @@
     appDir: "public_html",
     baseUrl: "js",
     dir: "public_html_optimized",
-    optimize: "closure",
+    removeCombined: true,
     modules: [
         {
             name: "main"
         }
     ],
-    closure:{
-        loggingLevel: "WARNING",
-        CompilationLevel: "SIMPLE_OPTIMIZATIONS",
-        CompilerOptions:{
-            languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.ECMASCRIPT5
+    optimize: "uglify2",
+    generateSourceMaps: false,
+    preserveLicenseComments: false,
+    uglify2:{
+        warnings:true,
+        mangle:false,
+        output:{
+            beautify:false
+        },
+        compress:{
+            sequences: true,
+            properties: true,
+            dead_code: true,
+            drop_debugger: true,
+            conditionals: true,
+            comparisons: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            hoist_funs: true,
+            if_return: true,
+            join_vars: true,
+            cascade: true,
+            side_effects: true,
+            warnings: true,
+            global_defs: {},
+            hoist_vars: false,
+            unsafe: false
         }
-    },
-    removeCombined: true
+    }
+
 })
