3efbbbad2521af944346a4449cd43f38a3b97ab1
Justin Loutsenhizer <nano351@gmail.com>
2014-04-05 01:58:51 -0400

    -clean up the generated build.txt after building the optimized version of the app

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 node r.js -o app.build.js
+rm public_html_optimized/build.txt #clean up build log
 #java -classpath js.jar:compiler.jar org.mozilla.javascript.tools.shell.Main r.js -o app.build.js
